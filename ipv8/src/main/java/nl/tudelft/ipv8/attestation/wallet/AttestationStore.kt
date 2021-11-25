3f1d4270e8b97491a73a76ba2f9c37398bf07247
Sharif Jacobino <sharif_jacobino@hotmail.com>
2021-11-25 14:55:11 +0100

    Serializable AttestationBlob

--- ipv8/src/main/java/nl/tudelft/ipv8/attestation/wallet/AttestationStore.kt
+++ ipv8/src/main/java/nl/tudelft/ipv8/attestation/wallet/AttestationStore.kt
@@ -3,7 +3,11 @@ package nl.tudelft.ipv8.attestation.wallet
 import nl.tudelft.ipv8.attestation.Authority
 import nl.tudelft.ipv8.attestation.WalletAttestation
 import nl.tudelft.ipv8.attestation.wallet.cryptography.bonehexact.BonehPrivateKey
+import nl.tudelft.ipv8.attestation.wallet.payloads.AttestationChunkPayload
 import nl.tudelft.ipv8.keyvault.PublicKey
+import nl.tudelft.ipv8.keyvault.defaultCryptoProvider
+import nl.tudelft.ipv8.messaging.*
+import nl.tudelft.ipv8.util.hexToBytes
 
 class AttestationBlob(
     val attestationHash: ByteArray,
@@ -13,7 +17,69 @@ class AttestationBlob(
     val metadata: String?,
     val signature: ByteArray?,
     val attestorKey: PublicKey?,
-)
+) : Serializable {
+    override fun serialize(): ByteArray {
+        // Keys are serialized using varlen here but they are fixed length
+        return (
+            attestationHash + serializeVarLen(blob) + serializeVarLen(key) + serializeVarLen(idFormat.toByteArray()) +
+            (
+                if (metadata != null && signature != null && attestorKey != null)
+                        serializeVarLen(metadata.toByteArray()) +
+                        serializeVarLen(signature) + serializeVarLen(attestorKey.keyToBin())
+                    else byteArrayOf()
+                    )
+            )
+    }
+
+    companion object Deserializer : Deserializable<AttestationBlob> {
+        override fun deserialize(buffer: ByteArray, offset: Int): Pair<AttestationBlob, Int> {
+            var localOffset = 0
+
+            val attestationHash = buffer.copyOfRange(offset + localOffset,
+                offset + localOffset + SERIALIZED_SHA1_HASH_SIZE)
+            localOffset += SERIALIZED_SHA1_HASH_SIZE
+
+            val (blob, blobSize) = deserializeVarLen(buffer, localOffset)
+            localOffset += blobSize
+
+            val (key, keySize) = deserializeVarLen(buffer, localOffset)
+            localOffset += keySize
+
+            val (idFormatBytes, idFormatSize) = deserializeVarLen(buffer, localOffset)
+            localOffset += idFormatSize
+
+            return if (buffer.lastIndex > offset + localOffset) {
+                val (metadataBytes, metadataSize) = deserializeVarLen(buffer, offset + localOffset)
+                localOffset += metadataSize
+
+                val (signature, signatureSize) = deserializeVarLen(buffer, offset + localOffset)
+                localOffset += signatureSize
+
+                val (attestorKey, attestorkeySize) = deserializeVarLen(buffer, offset + localOffset)
+                localOffset += attestorkeySize
+
+                val attestationBlob = AttestationBlob(
+                    attestationHash,
+                    blob,
+                    key,
+                    String(idFormatBytes),
+                    String(metadataBytes),
+                    signature,
+                    defaultCryptoProvider.keyFromPublicBin(attestorKey))
+                Pair(attestationBlob, localOffset)
+            } else {
+                val payload = AttestationBlob(
+                    attestationHash,
+                    blob,
+                    key,
+                    String(idFormatBytes),
+                    null, null, null)
+                Pair(payload, localOffset)
+            }
+
+        }
+    }
+}
 
 interface AttestationStore {
     fun getAllAttestations(): List<AttestationBlob>
